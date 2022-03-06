998be55669dbd5b2640df62f25d8fa3a2c9cfadd
ejham <ehfdl5252@naver.com>
2022-03-06 21:48:03 +0900

    Add OOP class11

---
+++ oop/Java100_oop_InheritanceSample5.java
@@ -0,0 +1,46 @@
+// getter, setter가 포함된 클래스의 상속을 코드로 구현해보시오.
+// 이 문제는 클래스의 상속을 구현 시 getter, setter의 개념과 용도를 알고 있는지를 묻는 문제이다.
+// 부모 클래스 --> Person, 자식 클래스 --> Villain, Hero
+
+class Person{
+    // Field
+    private String name;
+    private int age;
+    private int height;
+    private int weight;
+
+    // Constructor
+
+    // Method
+    public String getName() { return this.name; }
+    public void setName(String name) { this.name = name; }
+
+    public int getAge() { return this.age; }
+    public void setAge(int age) { this.age = age; }
+
+    public int getHeight() { return this.height; }
+    public void setHeight(int height) { this.height = height; }
+
+    public int getWeight() { return this.weight; }
+    public void setWeight(int weight) { this.weight = weight; }
+}
+
+class Villain extends Person{
+
+}
+
+class Hero extends Person{
+    
+}
+
+public class Java100_oop_InheritanceSample5 {
+    public static void main(String[] args){
+
+        // [1]: 객체 생성
+        Person p1 = new Person();
+        p1.setName("홍길동");
+        System.out.println(p1.getName());
+
+
+    }
+}
